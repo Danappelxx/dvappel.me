1da0fc57edae049c6b6dec0850298ff63d5827c8
Dan Appel <Dan.appel00@gmail.com>
2015-05-14 11:31:42 -0700

    move iron router methods to client

--- client/lib/environment.js
+++ client/lib/environment.js
@@ -17,3 +17,63 @@ Blog.config({
 	adminRole: 'blogAdmin',
 	authorRole: 'blogAuthor',
 });
+
+Router.configure({
+	loadingTemplate: 'loading',
+	notFoundTemplate: 'notFound',
+	layoutTemplate: 'layout'
+});
+
+Router.map(function () {
+
+	// Base site
+	this.route('home', {
+		path: '/'
+	});
+	this.route('resume', {
+		path: '/projects'
+	});
+	this.route('about', {
+		path: '/about'
+	});
+	this.route('contact', {
+		path: '/contact'
+	});
+	this.route('admin', {
+		path: '/admin',
+		template: 'accountsAdmin',
+		onBeforeAction: function() {
+			if(Meteor.loggingIn()) {
+				this.render(this.loadingTemplate);
+				this.next();
+			} else if(Roles.userIsInRole(Meteor.user(), ['admin'])) {
+				this.next();
+			} else {
+				this.redirect('/');
+				this.next();
+			}
+		}
+	});
+
+
+	// // Codepad
+	this.route('codepad-base', {
+		path: '/codepad',
+		template: 'codepadBase',
+		data: {
+			randDocid : function () {
+				var randDocid = Random.id(5);
+				return randDocid;
+			}
+		}
+	});
+	this.route('codepad', {
+		path: '/codepad/:padid',
+		template: 'codepad',
+		onBeforeAction: function () {
+			var padid = this.params.padid;
+			Session.set('padid', padid);
+			this.next();
+		}
+	});
+});
