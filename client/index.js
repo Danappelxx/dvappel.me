2e8f7603be166881a6bca3e953627ebe26ebe2b2
Dan Appel <dan.appel00@gmail.com>
2015-05-16 06:07:40 -0400

    add easter egg

--- client/index.js
+++ client/index.js
@@ -1,1 +1,13 @@
 // toplevel html's JS
+
+// EGGSAMPLE
+var egg = new Egg();
+
+egg
+	.AddCode("up,up,down,down,left,right,left,right,b,a", function() {
+		alert("Eggs!");
+	}, "easter-egg")
+	.AddHook(function(){
+		console.log("Hook called for: " + this.activeEgg.keys);
+		console.log(this.activeEgg.metadata);
+	}).Listen();
