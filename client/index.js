40b814200dec1a8a88d8dd4fc3dd7edbd90304bd
Dan Appel <Dan.appel00@gmail.com>
2015-05-18 21:19:01 -0700

    add easter egg

--- client/index.js
+++ client/index.js
@@ -1,11 +1,13 @@
 // toplevel html's JS
 
-// EGGSAMPLE
+// Easter egg
+
 var egg = new Egg();
 
 egg
 	.AddCode("up,up,down,down,left,right,left,right,b,a", function() {
 		alert("Eggs!");
+		location.href = 'https://atmospherejs.com/danappelxx/eggjs';
 	}, "easter-egg")
 	.AddHook(function(){
 		console.log("Hook called for: " + this.activeEgg.keys);
