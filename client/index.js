f560e3dbbb1a055d3c2a1f32b1f7d4db978ebca6
Dan Appel <Dan.appel00@gmail.com>
2015-05-18 21:30:40 -0700

    scope easter egg to projects page only

--- client/index.js
+++ client/index.js
@@ -1,15 +1,1 @@
 // toplevel html's JS
-
-// Easter egg
-
-var egg = new Egg();
-
-egg
-	.AddCode("up,up,down,down,left,right,left,right,b,a", function() {
-		alert("Eggs!");
-		location.href = 'https://atmospherejs.com/danappelxx/eggjs';
-	}, "easter-egg")
-	.AddHook(function(){
-		console.log("Hook called for: " + this.activeEgg.keys);
-		console.log(this.activeEgg.metadata);
-	}).Listen();
---
+++ client/views/main/resume.js
@@ -0,0 +1,13 @@
+// Easter egg
+
+var egg = new Egg();
+
+egg
+	.AddCode("up,up,down,down,left,right,left,right,b,a", function() {
+		alert("Eggs!");
+		location.href = 'https://atmospherejs.com/danappelxx/eggjs';
+	}, "easter-egg")
+	.AddHook(function(){
+		console.log("Hook called for: " + this.activeEgg.keys);
+		console.log(this.activeEgg.metadata);
+	}).Listen();
