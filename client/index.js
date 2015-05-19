227f392ca768e7a65adc22ef81c5029afe279f23
Dan Appel <dan.appel00@gmail.com>
2015-05-19 00:09:55 -0400

    add easter egg redirect

--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ var egg = new Egg();
 egg
 	.AddCode("up,up,down,down,left,right,left,right,b,a", function() {
 		alert("Eggs!");
+		location.href = 'https://atmospherejs.com/danappelxx/eggjs';
 	}, "easter-egg")
 	.AddHook(function(){
 		console.log("Hook called for: " + this.activeEgg.keys);
