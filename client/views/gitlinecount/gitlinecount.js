77d37b9e827857e7e7f27e10f93447fba16c55f7
Dan Appel <Dan.appel00@gmail.com>
2015-05-21 01:30:40 -0700

    update gitlinecounter (add progress bar, count all repositories, etc.), update bootstrap

--- client/views/gitlinecount/gitlinecount.js
+++ client/views/gitlinecount/gitlinecount.js
@@ -1,47 +1,94 @@
 Template.gitlinecount.events({
 	'click button': function () {
 
-		var repoOwner = $('#repo-owner-input').val();
-		var repoName = $('#repo-name-input').val();
-		var repoRequestUrl = 'https://api.github.com/repos/' + repoOwner + '/' + repoName;
-		var repoContentRequestUrl = repoRequestUrl + '/contents';
-
-		var fileCount = 0;
 		var files;
 		Session.set('lineCount', 0);
 
 		var countFile = function(file) {
 			var download_url = file.download_url;
-			Meteor.call('addFile', download_url);
 
 			Meteor.call('addFile', download_url, function (error, result) {
 				if(!error) {
 					var currLineCount = Session.get('lineCount');
 					var newLineCount = currLineCount + result;
 					Session.set('lineCount', newLineCount);
+					countedFiles++;
+					Session.set('percentCounted', Math.round((countedFiles/numFiles) * 100));
 				}
 			});
 
-			fileCount++;
 		};
 
 		var countDir = function (files) {
 
+			numFiles += files.length;
+
 			files.forEach( function (file) {
 				if (file.type == 'dir') {
-					$.getJSON(file.url, countDir);
+					Meteor.call('appendGithubClientStuffs', file.url, function (error, result) {
+						$.getJSON(result, countDir);
+					});
+					countedFiles++;
 				} else if (file.type == 'file') {
 					countFile(file);
 				}
 			});
 		};
 
-		$.getJSON(repoContentRequestUrl, countDir);
+		var countRepos = function (repos) {
+			repos.forEach(function (repo) {
+				var repoUrl = repo.contents_url.split('/{+path}')[0];
+				Meteor.call('appendGithubClientStuffs', repoUrl, function (error, result) {
+					$.getJSON(result, countDir);
+				});
+			});
+		};
+
+
+		var repoOwner = $('#repo-owner-input').val();
+		var repoName = $('#repo-name-input').val();
+
+		var repoRequestUrl;
+
+		var numFiles = 0;
+		var countedFiles = 0;
+
+
+		Session.set('isCounting', true);
+		Session.set('percentCounted', 0);
+
+		if( repoName === '') {
+			// No repository name -> count all repositories
+
+			repoRequestUrl = 'https://api.github.com/users/' + repoOwner + '/repos';
+			Meteor.call('appendGithubClientStuffs', repoRequestUrl, function (error, result) {
+				$.getJSON(result, countRepos);
+			});
+		} else {
+			// Yes repository name -> count just this repository
+
+			repoRequestUrl = 'https://api.github.com/repos/' + repoOwner + '/' + repoName;
+			var repoContentRequestUrl = repoRequestUrl + '/contents';
+			Meteor.call('appendGithubClientStuffs', repoContentRequestUrl, function (error, result) {
+				$.getJSON(result, countDir);
+			});
+		}
 	}
 });
 
 Template.gitlinecount.helpers({
 	currLineCount: function () {
 		return Session.get('lineCount');
+	},
+	isCounting: function () {
+		return (Session.get('percentCounted') < 101);
+	},
+	percentCounted: function () {
+		return Session.get('percentCounted');
 	}
 });
+
+Template.gitlinecount.rendered = function () {
+	$('#repo-name-input').tooltip({'trigger':'focus'});
+	// , 'title': 'You can leave this blank to get the number of lines in the whole account.'});
+};
--- server/methods.js
+++ server/methods.js
@@ -15,5 +15,18 @@ Meteor.methods({
 		}
 
 		return lineCount;
+	},
+	appendGithubClientStuffs: function (baseUrl) {
+		var CLIENT_ID = '79230b378d429cea8d72';
+		var CLIENT_SECRET = '315acf45721ff9aa756dc8fc9b19de036a668852';
+
+		if(baseUrl.endsWith('?ref=master')) {
+			baseUrl += '&';
+		} else {
+			baseUrl += '?';
+		}
+
+		var fullUrl = baseUrl + 'client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET;
+		return fullUrl;
 	}
 });
