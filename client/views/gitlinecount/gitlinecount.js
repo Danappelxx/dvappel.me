97662743f4a20cd7e6b1a4871bd77d855e419bdd
Dan Appel <Dan.appel00@gmail.com>
2015-05-20 21:12:00 -0700

    clean up code

--- client/views/gitlinecount/gitlinecount.js
+++ client/views/gitlinecount/gitlinecount.js
@@ -1,12 +1,10 @@
 Template.gitlinecount.events({
 	'click button': function () {
-		console.log('clicked!');
+
 		var repoOwner = $('#repo-owner-input').val();
 		var repoName = $('#repo-name-input').val();
 		var repoRequestUrl = 'https://api.github.com/repos/' + repoOwner + '/' + repoName;
 		var repoContentRequestUrl = repoRequestUrl + '/contents';
-
-		console.log(repoContentRequestUrl);
 
 		var fileCount = 0;
 		var files;
@@ -36,9 +34,6 @@ Template.gitlinecount.events({
 					countFile(file);
 				}
 			});
-
-			console.log(fileCount);
-			console.log(Session.get('lineCount'));
 		};
 
 		$.getJSON(repoContentRequestUrl, countDir);
--- server/methods.js
+++ server/methods.js
@@ -2,27 +2,6 @@
 Meteor.methods({
 	addFile: function (fileUrl) {
 		this.unblock();
-
-		// var Future = Npm.require("fibers/future");
-		// var f = new Future();
-
-		// HTTP.get(fileUrl, {timeout:3000}, function (error, result) {
-
-			// var currLineCount = Session.get('lineCount');
-			// var newLineCount = result.content.split('\n').length;
-			// var totalLineCount = currLineCount + newLineCount;
-
-			// ServerSession.set('lineCount', totalLineCount);
-
-			// f.return(response);
-			// var rawFile = response.content;
-			// var lineCount = rawFile.split('\n').length;
-			// // var currLineCount = Session.get('lineCount');
-			// // var newLineCount = currLineCount + lineCount;
-			// return lineCount;
-			// // Session.set('lineCount', newLineCount);
-		// });
-
 		var lineCount;
 
 		try {
@@ -36,18 +15,5 @@ Meteor.methods({
 		}
 
 		return lineCount;
-
-		// return f.wait();
 	}
 });
-
-		// try {
-		// 	var response = HTTP.get(fileUrl, {timeout: 3000});
-
-
-		// 	lineCount = 20;
-		// 	return lineCount;
-
-		// } catch (e) {
-		// 	return 1;
-		// }
