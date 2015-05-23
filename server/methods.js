935bb5b623442dd9428be0105a9fe57c5cc2d4dc
Dan Appel <Dan.appel00@gmail.com>
2015-05-23 13:42:53 -0700

    add gitignore to protect sensitive information

--- server/methods.js
+++ server/methods.js
@@ -17,8 +17,6 @@ Meteor.methods({
 		return lineCount;
 	},
 	appendGithubClientStuffs: function (baseUrl) {
-		var CLIENT_ID = '79230b378d429cea8d72';
-		var CLIENT_SECRET = '315acf45721ff9aa756dc8fc9b19de036a668852';
 
 		if(baseUrl.endsWith('?ref=master')) {
 			baseUrl += '&';
@@ -26,7 +24,7 @@ Meteor.methods({
 			baseUrl += '?';
 		}
 
-		var fullUrl = baseUrl + 'client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET;
+		var fullUrl = baseUrl + 'client_id=' + GITLINECOUNTER_CLIENT_ID + '&client_secret=' + GITLINECOUNTER_CLIENT_SECRET;
 		return fullUrl;
 	}
 });
