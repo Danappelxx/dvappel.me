54fc39ae1b9a0539233bfc40cc97c30f3a6ff0d7
Dan Appel <Dan.appel00@gmail.com>
2015-05-27 11:55:13 -0700

    fix divider parallax

--- client/views/divider/divider.js
+++ client/views/divider/divider.js
@@ -1,12 +1,7 @@
 Template.divider1.rendered = function () {
 	// $("#divider-1").stellar();
-};
-
-Template.divider2.rendered = function () {
-	// $("#divider-2").stellar();
-
 	if( window.innerWidth > 768) {
-		$.stellar({
+		$("#divider-1").stellar({
 			horizontalScrolling: false,
 			verticalScrolling: true,
 			horizontalOffset: 0,
@@ -15,7 +10,25 @@ Template.divider2.rendered = function () {
 			scrollProperty: 'scroll',
 			positionProperty: 'transform',
 			parallaxBackgrounds: true,
-			parallaxElements: false,
+			parallaxElements: true,
+			hideDistantElements: true,
+		});
+	}};
+
+Template.divider2.rendered = function () {
+	// $("#divider-2").stellar();
+
+	if( window.innerWidth > 768) {
+		$("#divider-2").stellar({
+			horizontalScrolling: false,
+			verticalScrolling: true,
+			horizontalOffset: 0,
+			verticalOffset: 0,
+			responsive: true,
+			scrollProperty: 'scroll',
+			positionProperty: 'transform',
+			parallaxBackgrounds: true,
+			parallaxElements: true,
 			hideDistantElements: true,
 		});
 	}
