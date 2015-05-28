28c1bf67e5bb7ee7d11dec8968b2ef04ad8e827e
Dan Appel <Dan.appel00@gmail.com>
2015-05-27 22:55:26 -0700

    add database structure ideas for chatroom

--- models/chatroom.js
+++ models/chatroom.js
@@ -1,1 +1,37 @@
+// Right now, it's just one chat room with a ton of messages:
+// 	Chat room
+// 		Messages
+// 			Message Content
+// 			Username
+// 			User id
+// 			Timestamp
+
+// What I want is something like:
+// Chat rooms
+// 		Chat Room
+// 			Messages
+// 				Message Content
+// 				Username
+// 				User id
+// 				Timestamp
+// 			Users online?
+// 				Username
+// 				User id
+
+// OR
+
+// Chat rooms
+// 		Chat room
+// 			Room Id
+// 			Users
+// Messages
+// 		Message
+// 			Message content
+// 			Room Id
+// 			Username
+// 			User Id
+// 			Timestamp
+
+// Probably going to go with the first method - seems simpler (and more efficient?)
+
 this.Chatroom = new Mongo.Collection('chatroom');
